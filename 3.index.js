9c36c09f2e69b790b38eb4158cd00ecd0040cb9d
KedarKandel <kandelkedar07@gmail.com>
2023-02-06 06:24:06 +0200

    cmt

--- 3.index.js
+++ 3.index.js
@@ -20,3 +20,33 @@ function secondNonRepetitiveNum(arr) {
 
 const arr = [1, 2, 3, 1, 2, 4,5];
 console.log(secondNonRepetitiveNum(arr));//0utputs 4 
+
+
+//////////////////
+
+
+//Given an array of numbers, write a JavaScript function to find the second largest number in the array.
+
+//brain storming
+
+const data = [1, 3, 5, 7, 9, 3, 9, 8, 50, 27];
+
+function secondLargestNum(data) {
+  let secondMax = 0;
+  let max = 0;
+  let obj = {};
+  for (let num of data) {
+    obj[num] = true;
+  }
+
+  for (let key in obj) {
+    if (parseInt(key) > secondMax) {
+      max = secondMax;
+      secondMax = key;
+    } else if (parseInt(key) > max) {
+      max = key;
+    }
+  }
+  return secondMax;
+}
+console.log(secondLargestNum(data));
