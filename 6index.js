13249ae5f4e7ed1df2319deff53f46b6f97ff115
KedarKandel <kandelkedar07@gmail.com>
2023-02-14 02:43:48 +0200

    solved

--- 6index.js
+++ 6index.js
@@ -1,4 +1,11 @@
-let A = [1, 2, -1, 3, 1, 4, 6];
+// Write a function:
+
+/*function solution(A)
+         that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+*/
+
+// let A = [1, 2, -1, 3, 1, 4, 6];
 
 function solution(A) {
   let positiveNumbers = A.filter((x) => x > 0);
@@ -13,8 +20,7 @@ function solution(A) {
   while (nums[smallestMissingPositive]) {
     smallestMissingPositive++;
   }
- 
+
   return smallestMissingPositive;
 }
 console.log(solution(A));
-
